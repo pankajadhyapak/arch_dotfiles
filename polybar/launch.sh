5075fafce56f0589e1c51be6915cc8b2f65890d6
Pankaj Adhyapak <pankaj.adhyapak@everphone.de>
2022-01-20 15:18:35 +0100

    polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,13 +4,35 @@
 killall -q polybar
 
 
-
+# Launch new polybar(s)
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
+    IFS=$'\n'  # must set internal field separator to avoid dumb
+    for entry in $(xrandr --query | grep " connected"); do
+        mon=$(cut -d" " -f1 <<< "$entry")
+        status=$(cut -d" " -f3 <<< "$entry")
+
+        tray_pos=""
+        if [ "$status" == "primary" ]; then
+            tray_pos="right"
+        fi
+
+        MONITOR=$mon TRAY_POS=$tray_pos polybar -r main & disown
+    done
+    unset IFS  # avoid mega dumb by resetting the IFS
 else
-  polybar --reload example &
+    polybar -r main 2>&1 | tee -a /tmp/polybar.log & disown
 fi
 
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
+#   polybar --reload example &
+# fi
+
 echo "Bars launched..."
+
+
+
