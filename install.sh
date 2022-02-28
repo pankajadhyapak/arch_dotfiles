c86f86f0a9fb19f4d3cf6fb6c6f809c2a610ff62
Pankaj Adhyapak <pankaj.adhyapak@everphone.de>
2022-02-28 20:22:54 +0100

    update

--- install.sh
+++ install.sh
@@ -109,13 +109,12 @@ ln -sf "$DOTFILES/scripts" "$HOME/.local/bin"
 chmod +x -R "$HOME/.local/bin/scripts"
 
 # wallpapers
-cp "$DOTFILES/wall.jpg" "$HOME/Pictures/wall4.jpg"
-betterlockscreen -u "$HOME/Pictures/wall4.jpg" --blur 0.5
+cp "$DOTFILES/wallpaper.jpg" "$HOME/Pictures/wallpaper.jpg"
+sudo "$DOTFILES/wallpaper.jpg" /usr/share/wall.jpg
 
 # guestures
 rm -rf "$XDG_CONFIG_HOME/libinput-gestures.conf"
 ln -sf "$DOTFILES/libinput-gestures.conf" "$XDG_CONFIG_HOME/libinput-gestures.conf"
-
 
 # mimeapps.list
 rm -rf "$XDG_CONFIG_HOME/mimeapps.list"
